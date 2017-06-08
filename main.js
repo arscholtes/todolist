c417d32342d47fe05ba89c9e0c1634db98fe4342
arscholtes <alex@ascholtes.me>
2017-06-08 16:07:29 -0400

    wow it works what!!!!!

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-// Add your JS here.
+var newTodo = document.querySelector( '#newTodo' );
+
+var todoList = document.getElementById('todoList');
+
+newTodo.addEventListener('keypress',
+function(event)
+{if (event.keyCode === 13){
+  var todoContent = event.target.value;
+  event.target.value = '';
+  var newTodoLi = document.createElement('li');
+newTodoLi.textContent = todoContent;
+todoList.appendChild(newTodoLi);
+}
+return event;
+}
+);
+
+
+
+
+// todoInput.addEventListener('keypress',
+// function(event)
+// {if (event.keyCode === 13){
+//   var todoContent = event.target.value;
+//   event.target.value = '';
+//   var newTodoLi = document.createElement('li');
+//   newTodoLi.textContent = todoContent;
+//   todoList.appendChild(newTodoLi);
+// }
+// return event;
+// }
+// );
